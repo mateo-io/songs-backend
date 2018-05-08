761914e45e3e43fffb66aab8592ae8a235295cc3
Mateo Mejia <mateomgj@gmail.com>
2018-05-07 19:49:15 -0500

    [create] migrations for core app
    Artists songs and lyrics. Relations are still TODO

---
+++ server/migrations/20180331053757-create-lyrics.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Lyrics', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      language: {
+        type: Sequelize.STRING
+      },
+      fetchedFrom: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Lyrics');
+  }
+};
